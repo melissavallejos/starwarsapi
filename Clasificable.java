c818ffb91832e5e3ca7aa89555220414ae234c18
melissavallejos <mvallejosch17@ucvvirtual.edu.pe>
2024-10-06 19:16:01 -0500

    Add files via upload

---
+++ Clasificable.java
@@ -0,0 +1,5 @@
+package com.vallejos.screenmatch.calculos;
+
+public interface Clasificable {
+    int getClasificacion();
+}
---
+++ Pelicula.java
@@ -0,0 +1,20 @@
+package com.vallejos.screenmatch.modelos;
+
+import com.vallejos.screenmatch.calculos.Clasificable;
+
+public class Pelicula extends Titulo implements Clasificable {
+    private String director;
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    @Override
+    public int getClasificacion() {
+        return (int) calculaMediaEvaluaciones() / 2;
+    }
+}
